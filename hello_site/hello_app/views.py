b98f68709897a619e7b2adf96d79a543fae1d6f9
Christopher Cialone <ccialone0929@gmail.com>
2022-12-11 23:30:50 -0500

    rendered

--- hello_site/hello_app/views.py
+++ hello_site/hello_app/views.py
@@ -1,4 +1,4 @@
-from django.views import View
+from django.views.generic import View
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -8,3 +8,12 @@ from django.http import HttpResponse
 class HelloWorldView(View):
     def get(self, request):
         return render(request, template_name='hello_world.html')
+
+
+class HelloView(View):
+    def get(self, request, name):
+        return render(
+            request, 
+            template_name='hello_name.html',
+            context={'person': name},
+        )
